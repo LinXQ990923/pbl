a2e87ff7b0d5992c3836f3617f08778ee427aa60
CrazyBlueStorm <3142037382@qq.com>
2020-06-19 15:08:19 +0800

    course controller

--- src/main/java/fudan/edu/pbl/controller/CourseController.java
+++ src/main/java/fudan/edu/pbl/controller/CourseController.java
@@ -1,16 +1,26 @@
 package fudan.edu.pbl.controller;
 
 
+import fudan.edu.pbl.entity.Course;
+import fudan.edu.pbl.entity.User;
+import fudan.edu.pbl.request.CreateCourseRequest;
 import fudan.edu.pbl.response.CourseDetailResponse;
 import fudan.edu.pbl.response.CourseResponse;
 import fudan.edu.pbl.response.ResultResponse;
+import fudan.edu.pbl.service.CourseService;
+import fudan.edu.pbl.service.impl.CourseServiceImpl;
+import fudan.edu.pbl.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.RequestResponseBodyMethodProcessor;
 
+import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -24,34 +34,80 @@ import java.util.List;
 @RestController
 public class CourseController {
 
+    @Autowired UserServiceImpl userService;
+    @Autowired CourseServiceImpl courseService;
+
     @RequestMapping(value = "/user/courses", method = RequestMethod.GET)
     public List<CourseResponse> getAllCourses(){
-        return null;
+        List<Course> courseList = courseService.list();
+        List<CourseResponse> coursesList = null;
+        for(int i = 0; i < courseList.size(); i++){
+            Course course = courseList.get(i);
+            CourseResponse courses = null;
+            courses.setId(course.getCourseID().toString());
+            courses.setName(course.getCourseName());
+            courses.setIntroduction(course.getIntroduction());
+            courses.setImage(course.getImgPath());
+            coursesList.add(courses);
+        }
+        return coursesList;
     }
 
     @RequestMapping(value = "/user/mycourses", method = RequestMethod.GET)
-    public List<CourseResponse> getMyCourses(){
-        return null;
+    public List<CourseResponse> getMyCourses(@RequestParam HttpSession session){
+        User user = userService.getByIdWithProperties(session.getAttribute("id").toString());
+        List<Course> courseList = user.getCourseList();
+        List<CourseResponse> coursesList = null;
+        for(int i = 0; i < courseList.size(); i++){
+            Course course = courseList.get(i);
+            CourseResponse courses = null;
+            courses.setId(course.getCourseID().toString());
+            courses.setName(course.getCourseName());
+            courses.setIntroduction(course.getIntroduction());
+            courses.setImage(course.getImgPath());
+            coursesList.add(courses);
+        }
+        return coursesList;
     }
 
     @RequestMapping(value = "/course/detail", method = RequestMethod.GET)
-    public List<CourseDetailResponse> getCourseDetail(){
-        return null;
+    public CourseDetailResponse getCourseDetail(@RequestParam(value = "course_id", required = true) String id){
+        Course course = courseService.getById(id);
+        return new CourseDetailResponse(course.getCourseID().toString(), course.getCourseName(),
+                course.getIntroduction(), userService.getById(course.getTeacherID()).getUserName(),
+                course.getEndTime().toString(), course.getImgPath(), "false");
     }
 
     @RequestMapping(value = "/course/add", method = RequestMethod.GET)
-    public ResultResponse addCourse(){
-        return null;
+    public ResultResponse addCourse(@RequestParam(value = "course_id", required = true) String id, HttpSession session){
+        userService.chooseCourse(Integer.parseInt(id), session.getId());
+        return new ResultResponse("true", "Add course to your course table successfully!");
     }
 
     @RequestMapping(value = "/course/create", method = RequestMethod.GET)
-    public ResultResponse createCourse(){
-        return null;
+    public ResultResponse createCourse(@RequestParam CreateCourseRequest request){
+        Course course = new Course();
+        course.setTeacherID(request.getTeacher_id());
+        course.setCourseName(request.getName());
+        course.setIntroduction(request.getDescription());
+        course.setStartTime(LocalDateTime.parse(request.getStart_time()));
+        course.setEndTime(LocalDateTime.parse(request.getEnd_time()));
+        course.setImgPath(request.getImage());
+        Boolean flag = courseService.save(course);
+        if(flag){
+            return new ResultResponse("true", "Create Course successfully!");
+        }else{
+            return new ResultResponse("false", "Create Course failed!");
+        }
     }
 
     @RequestMapping(value = "/course/delete", method = RequestMethod.GET)
-    public ResultResponse deleteCourse(){
-        return null;
+    public ResultResponse deleteCourse(@RequestParam(value = "course_id", required = true) String id){
+        Boolean flag = courseService.removeById(id);
+        if(flag){
+            return new ResultResponse("true", "Delete course successfully!");
+        }else{
+            return new ResultResponse("false", "Delete course failed!");
+        }
     }
-
 }
