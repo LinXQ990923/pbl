f98f325e3fb2e920a1b853b64ac027a0c22c8c73
LinXQ990923 <17302010044@fudan.edu.cn>
2020-06-20 01:32:21 +0800

    完成courseController测试和修改

--- src/main/java/fudan/edu/pbl/controller/CourseController.java
+++ src/main/java/fudan/edu/pbl/controller/CourseController.java
@@ -18,6 +18,7 @@ import org.springframework.web.servlet.mvc.method.annotation.RequestResponseBody
 
 import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,6 +39,7 @@ public class CourseController {
     @RequestMapping(value = "/user/courses", method = RequestMethod.GET)
     public List<CourseResponse> getAllCourses(){
         List<Course> courseList = courseService.list();
+        if (courseList==null)return null;
         List<CourseResponse> coursesList = new ArrayList<>();
         for(int i = 0; i < courseList.size(); i++){
             Course course = courseList.get(i);
@@ -53,28 +55,52 @@ public class CourseController {
 
     @RequestMapping(value = "/user/mycourses", method = RequestMethod.GET)
     public List<CourseResponse> getMyCourses(HttpSession session){
-        User user = userService.getByIdWithProperties(session.getAttribute("id").toString());
-        List<Course> courseList = user.getCourseList();
-        List<CourseResponse> coursesList = new ArrayList<>();
-        for(int i = 0; i < courseList.size(); i++){
-            Course course = courseList.get(i);
-            CourseResponse courses = new CourseResponse();
-            courses.setId(course.getCourseID().toString());
-            courses.setName(course.getCourseName());
-            courses.setIntroduction(course.getIntroduction());
-            courses.setImage(course.getImgPath());
-            coursesList.add(courses);
+        String user_id=session.getAttribute("id").toString();
+        List<Course> courseList = courseService.list();
+        if (courseList==null)return null;
+        List<CourseResponse> res = new ArrayList<>();
+        for (Course course:courseList){
+            if (userService.ifChooseCourse(course.getCourseID(),user_id)!=null){
+                CourseResponse tmp=new CourseResponse();
+                tmp.setId(course.getCourseID().toString());
+                tmp.setName(course.getCourseName());
+                tmp.setIntroduction(course.getIntroduction());
+                tmp.setImage(course.getImgPath());
+                res.add(tmp);
+            }
         }
-        return coursesList;
+        return res;
     }
 
+    @RequestMapping(value = "/teacher/courses/get", method = RequestMethod.GET)
+    public List<CourseResponse> getTeacherCourses(HttpSession session){
+        String teacher_id=session.getAttribute("id").toString();
+        List<Course> courseList = courseService.list();
+        if (courseList==null)return null;
+        List<CourseResponse> res = new ArrayList<>();
+        for (Course course:courseList){
+            if (course.getTeacherID().equals(teacher_id)){
+                CourseResponse tmp=new CourseResponse();
+                tmp.setId(course.getCourseID().toString());
+                tmp.setName(course.getCourseName());
+                tmp.setIntroduction(course.getIntroduction());
+                tmp.setImage(course.getImgPath());
+                res.add(tmp);
+            }
+        }
+        return res;
+    }
+
+
     @RequestMapping(value = "/course/detail", method = RequestMethod.GET)
-    public CourseDetailResponse getCourseDetail(@RequestParam(value = "course_id", required = true) String id){
+    public CourseDetailResponse getCourseDetail(@RequestParam(value = "course_id", required = true) String id,HttpSession session){
         Course course = courseService.getById(id);
+        DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         if(course != null){
+            String isAdd=userService.ifChooseCourse(course.getCourseID(),session.getAttribute("id").toString())==null?"false":"true";
             return new CourseDetailResponse(course.getCourseID().toString(), course.getCourseName(),
                     course.getIntroduction(), userService.getById(course.getTeacherID()).getUserName(),
-                    course.getEndTime().toString(), course.getImgPath(), "false");
+                    dtf2.format(course.getEndTime()), course.getImgPath(), isAdd);
         }else{
             return null;
         }
@@ -82,20 +108,24 @@ public class CourseController {
 
     @RequestMapping(value = "/course/add", method = RequestMethod.GET)
     public ResultResponse addCourse(@RequestParam(value = "course_id", required = true) String id, HttpSession session){
-        userService.chooseCourse(Integer.parseInt(id), session.getId());
+        userService.chooseCourse(Integer.parseInt(id), session.getAttribute("id").toString());
         return new ResultResponse("true", "Add course to your course table successfully!");
     }
 
-    @RequestMapping(value = "/course/create", method = RequestMethod.GET)
-    public ResultResponse createCourse(@RequestBody CreateCourseRequest request){
+    @RequestMapping(value = "/course/create", method = RequestMethod.POST)
+    public ResultResponse createCourse(@RequestBody CreateCourseRequest request,HttpSession session){
         Course course = new Course();
-        course.setTeacherID(request.getTeacher_id());
+        course.setTeacherID(session.getAttribute("id").toString());
         course.setCourseName(request.getName());
         course.setIntroduction(request.getDescription());
-        course.setStartTime(LocalDateTime.parse(request.getStart_time()));
-        course.setEndTime(LocalDateTime.parse(request.getEnd_time()));
+        LocalDateTime start=LocalDateTime.now();
+        int year=Integer.parseInt(request.getEnd_time().split("-")[0]);
+        int month=Integer.parseInt(request.getEnd_time().split("-")[1]);
+        int day=Integer.parseInt(request.getEnd_time().split("-")[2]);
+        course.setStartTime(start);
+        course.setEndTime(LocalDateTime.of(year,month,day,start.getHour(),start.getMinute()));
         course.setImgPath(request.getImage());
-        Boolean flag = courseService.save(course);
+        boolean flag = courseService.save(course);
         if(flag){
             return new ResultResponse("true", "Create Course successfully!");
         }else{
